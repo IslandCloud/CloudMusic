b1b71dca9d92ad937f8ec6a5051d7f555dd76017
Junwen <hjw970821@gmail.com>
2017-09-06 18:09:48 +0800

    initial commit

---
+++ Hot50Songs.py
@@ -0,0 +1,47 @@
+#输入歌手id，返回该歌手热门50单曲及单曲id
+
+import requests
+import bs4
+
+id = 12140247
+url = 'http://music.163.com/artist?id={}'.format(id)
+
+def get_html(url):
+    headers = {
+        'Referer': 'http://music.163.com/',
+        'Host': 'music.163.com',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:38.0) Gecko/20100101 Firefox/38.0 Iceweasel/38.3.0',
+        'Accept': 'text/html,application/xhml+xml,application/xml;q=0.9,*/*;q=0.8'
+    }
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+        r.encoding = 'utf-8'
+        return r.text
+    except:
+        return 'get_html error'
+
+
+def get_content(url):
+    try:
+        songs = []
+        html = get_html(url)
+        soup = bs4.BeautifulSoup(html, 'lxml')
+        allSongs = soup.find('ul', class_='f-hide')
+        for song in allSongs.find_all('a'):
+            details = {}
+            details['name'] = song.text
+            details['link'] = song['href']
+            songs.append(details)
+        return songs
+    except:
+        return 'get content error'
+
+
+def print_content(url):
+    songs = get_content(url)
+    for song in songs:
+        print('{} : {}'.format(song['name'], song['link']))
+
+
+print_content(url)
